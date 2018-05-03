2592994660fac31201d9d93f441a49591bf909e5
corytveten <corytveten@gmail.com>
2018-05-03 16:16:00 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -16,5 +16,5 @@ def sort_array_char_count(numbers)
 end
 
 def swap_elements(numbers)
-  numbers[0], numbers[1], numbers[2] = numbers[0]. numbers[2], numbers[1]
+  numbers[1], numbers[2] = numbers[2], numbers[1]
 end
